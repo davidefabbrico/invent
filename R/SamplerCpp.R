7fec12c3bd9ba67670d4bac04a4b8d668974ffe5
davide fabbrico <davide.fabbrico@unifi.it>
2024-04-10 15:34:30 +0100

    update contingency table variable selection

--- R/SamplerCpp.R
+++ R/SamplerCpp.R
@@ -1,12 +1,38 @@
+my_cm <- function(sel, true) {
+  tp <- 0
+  tn <- 0
+  fp <- 0
+  fn <- 0
+  
+  p <- length(sel)
+  
+  for (j in 1:p) {
+    if ((sel[j] == true[j]) & (true[j] == 1)) {
+      tp <- tp + 1
+    }
+    if ((sel[j] == true[j]) & (true[j] == 0)) {
+      tn <- tn + 1
+    }
+    if ((sel[j] != true[j]) & (true[j] == 0)) {
+      fp <- fp + 1
+    }
+    if ((sel[j] != true[j]) & (true[j] == 1)) {
+      fn <- fn + 1
+    }
+  }
+  contTable <- matrix(c(tn, fp, fn, tp), nrow = 2, ncol = 2)
+  return(contTable)
+}
+
 #' invMCMC
 #' 
 #' @export
 
 ##### ------------------------------------------------------------------ ######
 invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0.00025, 0.4, 1.6, 0.2, 1.8, 0.4, 1.6, 0.2, 1.8), 
-                   mht = c(1.4, 0.8, 1, 0.3, 0.7, 0.4, 4, 2.5), 
-                   rank = 0.95, iter = 10000, burnin = iter/2, thin = 5, ha = 2, 
-                   detail = FALSE, data = NULL, pb = TRUE) {
+                    mht = c(1.4, 0.8, 1, 0.3, 0.7, 0.4, 4, 2.5), 
+                    rank = 0.95, iter = 10000, burnin = iter/2, thin = 5, ha = 2, 
+                    detail = FALSE, data = NULL, pb = TRUE) {
   
   result <- NULL
   #useful quantities
@@ -61,56 +87,6 @@ invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0
     X_nl <- X_nlin
     X_val_nl <- matrix(0, nrow = 0, ncol = 0)
   }
-  
-  # Prediction
-  # build our basis p spline representation
-  # X_val <- vector()
-  # X_val_l <- vector()
-  # X_val_nl <- vector()
-  # d_val <- vector()
-  # 
-  # if (!is.null(y_val)) {
-  #   inD_val <- 1
-  #   for(j in 1:p) {
-  #     xj_val <- x_val[,j]
-  #     if (any(xj_val != as.integer(xj_val))) {
-  #       X_val <- cbind(X_val, xj_val)
-  #       xjl_val <- lin(xj_val)
-  #       X_val_l <- cbind(X_val_l, xjl_val)
-  #       xjtilde_val <- sm(x = xj_val, rankZ = rank) 
-  #       X_val_nl <- cbind(X_val_nl, xjtilde_val)
-  #       d_val[inD_val] <- dim(xjtilde_val)[2]
-  #       inD_val <- inD_val + 1
-  #     }       
-  #   }
-  #   inD <- 1
-  #   for(j in 1:p) {
-  #     xj <- x[,j]
-  #     if (any(xj != as.integer(xj))) {
-  #       X <- cbind(X, xj)
-  #       xjl <- lin(xj)
-  #       X_l <- cbind(X_l, xjl)
-  #       xjtilde <- sm(x = xj, rankZ = rank) 
-  #       X_nl <- cbind(X_nl, xjtilde)
-  #       d[inD] <- dim(xjtilde)[2]
-  #       inD <- inD + 1
-  #     }       
-  #   }
-  #   # linear covariates transformation
-  #   for(j in 1:p) {
-  #     xj_val <- x_val[,j]
-  #     if (all(xj_val == as.integer(xj_val))) {
-  #       X_val <- cbind(X_val, xj_val)
-  #       # n_cat <- n_cat + 1
-  #       X_val_l <- cbind(X_val_l, xj_val)
-  #     }   
-  #   }
-  #   cd_val <- c(0, cumsum(d_val))
-  #   q_val <- sum(d_val)
-  # } else {
-  #   cd_val <- 0
-  #   q_val <- 0
-  # }
   
   # Call the C++ function
   result = bodyMCMC(as.vector(y), as.integer(p), as.integer(nobs), as.vector(cd),
@@ -169,7 +145,7 @@ invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0
     # true non null effect
     true_MainLinear <- as.numeric(data$alpha_0_l[1,] != 0)
     # table linear main effect
-    tab_MainLinear <- matrix(table(sel_MainLinear, true_MainLinear), nrow = 2, ncol = 2)
+    tab_MainLinear <- my_cm(sel_MainLinear, true_MainLinear)
     matt_MainLinear <- mcc(confusionM = tab_MainLinear)
     tpr_MainLinear <- tab_MainLinear[2,2]/sum(tab_MainLinear[,2]) 
     fpr_MainLinear <- tab_MainLinear[2,1]/sum(tab_MainLinear[,1])
@@ -179,10 +155,18 @@ invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0
     # true non null effect
     true_MainNonLinear <- as.numeric(data$alpha_0_tilde[1,] != 0)
     # table linear main effect
-    tab_MainNonLinear <- matrix(table(sel_MainNonLinear, true_MainNonLinear), nrow = 2, ncol = 2)
+    tab_MainNonLinear <- my_cm(sel_MainNonLinear, true_MainNonLinear)
     matt_MainNonLinear <- mcc(confusionM = tab_MainNonLinear)
-    tpr_MainNonLinear <- tab_MainNonLinear[2,2]/sum(tab_MainNonLinear[,2]) 
-    fpr_MainNonLinear <- tab_MainNonLinear[2,1]/sum(tab_MainNonLinear[,1])
+    if (sum(tab_MainNonLinear[,2]) == 0) {
+      tpr_MainNonLinear <- 0
+    } else {
+      tpr_MainNonLinear <- tab_MainNonLinear[2,2]/sum(tab_MainNonLinear[,2]) 
+    }
+    if (sum(tab_MainNonLinear[,1]) == 0) {
+      fpr_MainNonLinear <- 0
+    } else {
+      fpr_MainNonLinear <- tab_MainNonLinear[2,1]/sum(tab_MainNonLinear[,1])
+    }
     ############### Linear Interaction effect ###################
     sel_IntLinear <- mppi_IntLinear > 0.5
     sel_IntLinear[sel_IntLinear == TRUE] = 1
@@ -190,7 +174,7 @@ invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0
     true_IntLinear[true_IntLinear == TRUE] = 1
     Vec_sel_IntLinear <- sel_IntLinear[upper.tri(sel_IntLinear)]
     Vec_true_IntLinear <- true_IntLinear[upper.tri(true_IntLinear)]
-    tab_IntLinear <- matrix(table(Vec_sel_IntLinear, Vec_true_IntLinear), nrow = 2, ncol = 2)
+    tab_IntLinear <- my_cm(Vec_sel_IntLinear, Vec_true_IntLinear)
     matt_IntLinear <- mcc(confusionM = tab_IntLinear)
     tpr_IntLinear <- tab_IntLinear[2,2]/sum(tab_IntLinear[,2]) 
     fpr_IntLinear <- tab_IntLinear[2,1]/sum(tab_IntLinear[,1])
@@ -208,7 +192,7 @@ invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0
     }
     Vec_sel_IntNonLinear <- sel_IntNonLinear[upper.tri(sel_IntNonLinear)]
     Vec_true_IntNonLinear <- true_IntNonLinear[upper.tri(true_IntNonLinear)]
-    tab_IntNonLinear <- matrix(table(Vec_sel_IntNonLinear, Vec_true_IntNonLinear), nrow = 2, ncol = 2)
+    tab_IntNonLinear <- my_cm(Vec_sel_IntNonLinear, Vec_true_IntNonLinear)
     matt_IntNonLinear <- mcc(confusionM = tab_IntNonLinear)
     tpr_IntNonLinear <- tab_IntNonLinear[2,2]/sum(tab_IntNonLinear[,2]) 
     fpr_IntNonLinear <- tab_IntNonLinear[2,1]/sum(tab_IntNonLinear[,1])
@@ -218,7 +202,7 @@ invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0
     trueInd <- c(as.numeric(true_MainLinear), as.numeric(true_MainNonLinear), 
                  Vec_true_IntLinear, Vec_true_IntNonLinear)
     ##########  aggregate
-    contTable <- matrix(table(selectInd, trueInd), nrow = 2, ncol = 2)
+    contTable <- my_cm(selectInd, trueInd)
     matt <- mcc(confusionM = contTable)
     tpr <- contTable[2,2]/sum(contTable[,2]) 
     fpr <- contTable[2,1]/sum(contTable[,1]) 
@@ -234,11 +218,6 @@ invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0
   # Choice to have the details or not
   if (detail == TRUE) {
     res <- result
-    # res <- list(d = d, linear_predictor = result$linear_predictor,
-    #             beta_l = result$beta_l, beta_nl = result$beta_nl, alpha_0_l = result$alpha_0_l,
-    #             alpha_0_nl = result$alpha_0_nl, alpha_star_l = result$alpha_star_l, alpha_star_nl = result$alpha_star_nl,
-    #             gamma_0_l = result$gamma_0_l, gamma_0_nl = result$gamma_0_nl, gamma_star_l = result$gamma_star_l,
-    #             gamma_star_nl = result$gamma_star_nl, sigma = result$sigma, LogLikelihood = result$LogLikelihood, y_oos = result$y_oos)
   } else {
     if (is.null(data)) {
       # if there is no data generating mechanism
@@ -255,8 +234,6 @@ invMCMC <- function(y, x, y_val = NULL, x_val = NULL, hyperpar = c(5, 5, 5, 5, 0
                     mppi_IntNonLinear = mppi_IntNonLinear, execution_time = execution_time, acc_rate = acc_rate, sigma = sigma)
       }
     } else {
-      # vorrei riportare anche tpr, fpr e matt separati per effetto
-      # lineare e non lineare
       if (!is.null(y_val)) {
         # return tpr, fpr, matt
         res <- list(linear_predictor = yhat, y_OutSample = y_tilde, y_tComp = y_tComplete,
